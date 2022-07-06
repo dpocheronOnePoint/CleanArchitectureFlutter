6a6a298bf5c8c72024260ed1d9de123c5cc478ac
your_username <your_email@gmail.com>
2022-07-06 10:41:18 +0200

    initialize project

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,9 @@
+const String kMaterialAppTitle = 'Flutter Clean Architecture';
+
+// API
+const String kBaseUrl = 'https://newapi.org/v2';
+const String kApiKey = 'Your Api KEy';
+
+// Database
+const String kArticlesTableName = 'article_table';
+const String kDatabaseName = 'app_database.db';
